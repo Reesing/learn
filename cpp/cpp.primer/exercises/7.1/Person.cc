6fb8fc3c1df1b2016163815319570826a3553c93
Reesing <lixiang.reesing@163.com>
2019-03-11 23:31:07 +0800

    7.1

--- cpp/cpp.primer/exercises/7.1/Person.cc
+++ cpp/cpp.primer/exercises/7.1/Person.cc
@@ -9,9 +9,23 @@ struct Person {
 
     string name;
     string address;
+};
+
+istream &read(istream &is,Person &person){
+   is >> person.name >> person.address;
+   return is;
+}
+
+ostream &print(ostream &os,const Person &person){
+    os << "Name: " << person.name << ", address: " << person.address << endl;
+    return os;
 }
 
 int main(){
+    Person a;
+    while(read(cin,a))
+        print(cout,a);
+
     return 0;
 }
 
--- cpp/cpp.primer/exercises/7.1/Sales_data.cc
+++ cpp/cpp.primer/exercises/7.1/Sales_data.cc
@@ -1,32 +1,53 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
 struct Sales_data {
-    string isbn() {return bookNo;};
-    Sales_data combine(Sales_data &);
+    std::string isbn() const{return bookNo;};
+    Sales_data &combine(const Sales_data &);
+    double avgPrice() const;
 
-    string bookNo;
+    std::string bookNo;
     unsigned units_sold = 0;
     double revenue;
 };
 
-Sales_data Sales_data::combine(Sales_data &sale)
+Sales_data &Sales_data::combine(const Sales_data &sale)
 {
     units_sold += sale.units_sold;
     revenue += sale.revenue;
     return *this;
 }
 
+double Sales_data::avgPrice() const{
+    if (units_sold>0)
+        return revenue/units_sold;
+    else
+        return 0;
+}
+
+std::istream &read(std::istream &is,Sales_data &item){
+    double price;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = item.units_sold*price;
+    return is;
+}
+
+std::ostream &print(std::ostream &os, const Sales_data &item){
+    //os << item.isbn() << " " << item.units_sold << " " << item.revenue;
+    os << item.isbn() << " " << item.units_sold << " " << item.revenue << " " << item.avgPrice();
+    return os;
+}
 
 int main()
 {
     Sales_data total,temp;
-    double price;
-    if (cin >> total.bookNo >> total.units_sold >> price)
+    //double price;
+    //if (cin >> total.bookNo >> total.units_sold >> price)
+    if (read(std::cin,total))
     {
-        total.revenue = total.units_sold*price;
-        while (cin >> temp.bookNo >> temp.units_sold >> price)
+        //total.revenue = total.units_sold*price;
+        //while (cin >> temp.bookNo >> temp.units_sold >> price)
+        while (read(std::cin,temp))
         {
             if (total.isbn()==temp.isbn())
             {
@@ -34,14 +55,17 @@ int main()
             }
             else
             {
-                cout << total.bookNo << " " << total.units_sold << " " << total.revenue << endl; 
+                print(std::cout,total) << std::endl;
+                //cout << total.bookNo << " " << total.units_sold << " " << total.revenue << endl; 
+                total = temp;
             }
         }
-        cout << total.bookNo << " " << total.units_sold << " " << total.revenue << endl; 
+        //cout << total.bookNo << " " << total.units_sold << " " << total.revenue << endl; 
+        print(std::cout,total) << std::endl;
     }
     else
     {
-        cerr << "No new data!" << endl;
+        std::cerr << "No new data!" << std::endl;
         return -1;
     }
     return 0;
